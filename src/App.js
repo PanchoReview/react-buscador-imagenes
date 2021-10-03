dfe55d05025aa04fee217a24d2eafefa866c521d
Francisco Astorga <fastorga@nimok.com>
2021-10-03 13:52:03 -0300

    Implemented call to unsplashed api on submit. Results stored in state.
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { Formik, Form, Field } from "formik";
+import { useState } from "react";
 import './header.css'
 
 
 const App = () => {
+  const [photos, setPhotos] = useState()
+  console.log(photos)
   return (
     <div>
       <header>
         <Formik
           initialValues={{ search: '' }}
-          onSubmit={async values => {
-            //llamar a api unsplash
-            console.log(values)
+          onSubmit={async values => {            
+            const api= `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`            
+            const response = await fetch(api, {
+              headers: {
+                'Authorization': 'Client-ID cn7oPtcOEcDjCFAI844KhChYCZNDeHum3j1eJMVN6zc'
+              }
+            })
+
+            const data = await response.json()            
+            setPhotos(data.results)
           }}
         >
           <Form>
